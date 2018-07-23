40a279c5b0762763b498f0b2f2f7a4a3a41d56a6
Shannon Egan ( Students - Vacation ) <egan@pplxint9.physics.ox.ac.uk>
2018-07-23 15:58:13 +0100

    Added dp/p ~ cp smearing method and designed python program to loop over however many sets of pT-varying parameters it is offered

--- chi2fitting.py
+++ chi2fitting.py
@@ -1,23 +1,131 @@
 #from ROOT import gROOT as R
 #R.SetBatch(True) 
+#import numpy as np
 import ROOT as R
-input = R.TFile.Open("./rootfiles/create_templates.root", "UPDATE")
+import csv
+from decimal import *
+import matplotlib.pyplot as plt
 
-chi2Plot = input.Get("chi2 plot")
+input = R.TFile.Open("./rootfiles/build_compare_templates.root", "UPDATE")
 
-quadfit = R.TF1("chi2_quadfit", "[0]+[2]*(x-[1])**2", 79.8, 80.8)
-quadfit.SetParameters(33,80.4,10000)
+pTParams = [[]]
+Wmass_pred = [[]*2]
+Wmass_err = [[]*2]
+chi2min_fit = [[]*2]
 
-chi2Plot.Fit("chi2_quadfit")
+quadfit = R.TF1("chi2_quadfit", "[0]+(1/([2]**2))*(x-[1])**2", 79.8, 80.8)
+quadfit.SetParName(0,"MinChi2")
+quadfit.SetParName(1,"WMass")
+quadfit.SetParName(2,"WMassUncertainty")
 
-chi2min = quadfit.GetMinimum()
-Mw = quadfit.GetX(chi2min)
-MwSigma = abs(quadfit.GetX(chi2min+1)-Mw)
 
-print('The best W mass is ' + str(Mw) + '+/-' + str(MwSigma))
+firstrow = True 
+with open('./pTparameters.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter = ',', quoting=csv.QUOTE_NONNUMERIC)
+    for row in readCSV:
+        if (firstrow): 
+            pTParams[0] = row
+            firstrow = False
+        else:
+            pTParams.append(row)
 
-c = R.TCanvas()
-chi2Plot.Draw("AP")
+print(pTParams)
+print len(pTParams)
+print(pTParams[1])
 
-c.Print("./plots/chi2Plot.png")
-c.Close()
+
+for toyset in range (0,2):
+    print('Now plotting set of toys: ' + str(toyset))
+    ntoys = len(pTParams[toyset])
+    for toyit in range (0, ntoys):    
+
+        chi2Plot_name = "chi2plot" + str(toyset) + str(toyit)    
+        print("Loading graph: " + chi2Plot_name)
+        chi2Plot = input.Get(chi2Plot_name)
+        print(type(chi2Plot))
+    
+        chi2stats = chi2Plot.GetY()
+    
+        p0_init = min(chi2stats)
+        print("p0_init: " + str(p0_init))
+        p1_init = 80.5
+        p2_init = 0.01
+    
+    #print(chi2max)
+    #print("Spread of chi2 results: " + str(chi2max-p0_init))
+    #p2_init = (chi2max-p0_init)/(79.8-p1_init)
+    #print("Initiating parameter 2 as: " + str(p2_init))
+    #p2_init = 1/(abs(p2_init))
+
+        quadfit.SetParameters(p0_init, p1_init, p2_init)
+
+        chi2Plot.Fit("chi2_quadfit")
+
+    #chi2min = quadfit.GetMinimum()
+        Mw = quadfit.GetParameter(1)
+        MwSigma = quadfit.GetParameter(2)
+
+        print('The best W mass is ' + str(Mw) + '+/-' + str(MwSigma))
+        
+        try:
+            Wmass_pred[toyset].append(Mw)
+            Wmass_err[toyset].append(MwSigma)
+            chi2min_fit[toyset].append(quadfit.GetParameter(0))
+        except:
+            Wmass_pred.append([Mw])
+            Wmass_err.append([MwSigma])
+            chi2min_fit.append([quadfit.GetParameter(0)])
+            
+        
+    #pTParams.append(toyit*pTincrement)
+
+        c = R.TCanvas()
+        chi2Plot.Draw("AP")
+
+    #TLegend *legend = new TLegend()
+    #legend->SetHeader()
+    #legend->Draw()
+
+        chi2image = "./plots/chi2Plot" + str(toyset) +  str(toyit) + "_fit.png"
+        c.Print(chi2image)
+        c.Close()
+
+    '''
+    if not ((len(Wmass_pred) == len(Wmass_err)) and (len(Wmass_pred) == len(chi2min_fit)) and (len(Wmass_pred) == len(pTParams))):
+        print("Warning!  The arrays you wish to plot are not of equal length!")
+        print('Wmass predictions: ' + str(len(Wmass_pred)))
+        print('Wmass errors: ' + str(len(Wmass_err)))
+        print('pT parameters: ' + str(len(pTParams)))
+        print('chi2 test statistics: ' + str(len(chi2min_fit)))
+    '''
+
+fig1, (axW, axChi) = plt.subplots(2, sharex=True)
+axW.scatter(pTParams[0], Wmass_pred[0])
+axW.errorbar(pTParams[0], Wmass_pred[0], yerr=Wmass_err[0], fmt='o')
+axW.set_ylabel('Predicted mass of W (GeV)')
+
+max1 = max(pTParams[0]) + 0.1*max(pTParams[0])
+min1 = -0.1*max(pTParams[0])
+axW.set_xlim([min1,max1])
+
+axChi.scatter(pTParams[0], chi2min_fit[0])
+axChi.set_ylabel('Test stat. of W template fit')
+axChi.set_xlabel('Mean percent pT smear (Gaussian)')
+
+fig1.savefig("./plots/gaus_pT_smear.png")
+
+
+fig2, (ax2W, ax2Chi) = plt.subplots(2, sharex=True)
+ax2W.scatter(pTParams[1], Wmass_pred[1])
+ax2W.errorbar(pTParams[1], Wmass_pred[1], yerr=Wmass_err[1], fmt='o')
+ax2W.set_ylabel('Predicted mass of W (GeV)')
+
+max2 = max(pTParams[1]) + 0.1*max(pTParams[1])
+min2 = -0.1*max(pTParams[1])
+ax2W.set_xlim([min2,max2])
+
+ax2Chi.scatter(pTParams[1], chi2min_fit[1])
+ax2Chi.set_ylabel('Test stat. of W template fit')
+ax2Chi.set_xlabel('Mean percent pT smear (Gaussian)')
+
+fig2.savefig("./plots/gaus_pTdependent_smear.png")
--- src/build_compare_histograms.cxx
+++ src/build_compare_histograms.cxx
@@ -25,19 +25,41 @@
 #include "TH2F.h"
 #include <vector>
 #include "TemplateStruct.h"
-
+#include "TRandom.h"
+#include <fstream>
 
 using namespace std;
 
+/*
+void write_vector (ofstream bufferstream, vector<Double_t> data_vect) {
+
+  if (!bufferstream.is_open()) {
+      cout << "ERROR: ofstream not associated to open file" << endl;  
+  } else {
+    for (vector<Double_t>::iterator data_it = data_vect.begin(); data_it != data_vect.end(); ++data_it) { 
+      if (data_it == data_vect.begin()) {
+	bufferstream << *data_it; 
+      } else {
+	bufferstream << ',' << *data_it; 
+      }
+    }
+    cout << '\n';
+  }
+}
+*/ 
+
 int main(){
   int hist_dims[3] = {40,30,50};
-  int ndata_files = 3;
+  int ndata_files = 1;
   int ntemplates = 11;
+  int ntoys = 6;
   Double_t Mnom = 80.4;
   Double_t gamma = 2.15553;
   
-  vector<TH1F *> toys, templates;
-  vector<Double_t> Wmasses, pTparams;
+  vector<TH1F *> templates;
+  vector< vector<TH1F *> > toys(2);
+  vector<Double_t> Wmasses;
+  vector< vector<Double_t> > pTparams(3);
 
   vector<TH1F *>::iterator toyit, templateit;
   vector<Double_t>::iterator Wmassit, pTparamsit;
@@ -45,9 +67,6 @@ int main(){
   string output_name = "~/oxford-lhcb-wmass/rootfiles/build_compare_templates.root";
 
   TH1::SetDefaultSumw2();
-
-  //define histogram
-  TH1F *h_muPT= new TH1F("h_mu_PT","#mu P_{T}",hist_dims[0], hist_dims[1], hist_dims[2]);
 
   //create output file  	
   TFile *output = new TFile(output_name.c_str(),"RECREATE");
@@ -70,11 +89,15 @@ int main(){
   MCDecayTree->SetBranchAddress("mu_PT", &mu_PT);
   MCDecayTree->SetBranchAddress("prop_M", &prop_M);
 
+  MCDecayTree->SetBranchStatus("*", 0);
+  MCDecayTree->SetBranchStatus("mu_PT", 1);
+  MCDecayTree->SetBranchStatus("prop_M",1);
 
   stringstream snominal, sreweight;
   snominal << fixed << setprecision(2) << Mnom;
   string template_name;
-  string toy_name = "pTsmear";
+  string toy_name1 = "GausSmear";
+  string toy_name2 = "GausSmear_pTdependent";
 
   cout << "error marker 1" << endl;
 
@@ -91,50 +114,240 @@ int main(){
     sreweight.str(string());
   }
 
-  for (Double_t pTparam = 1.0; pTparam <=1.0; pTparam += 0.5) {
-    pTparams.push_back(pTparam);
-    TH1F *hpT_toy = new TH1F(toy_name.c_str(), "mu_PT", hist_dims[0], hist_dims[1], hist_dims[2]);
-    toys.push_back(hpT_toy);
-    toy_name = "";
+  cout << "error marker 2" << endl;
+  
+  for (Double_t pTparam = 0.0; pTparam <=0.03; pTparam += (0.03)/(ntoys-1)) {
+    pTparams[0].push_back(pTparam);
+    toy_name1 = toy_name1 + to_string(pTparam);
+    TH1F *hpT_toy = new TH1F(toy_name1.c_str(), "mu_PT with simple momentum smear", hist_dims[0], hist_dims[1], hist_dims[2]);
+    toys[0].push_back(hpT_toy);
+    toy_name1 = "GausSmear";
   }
+
+  cout << "error marker 3" << endl;
+
+  for (Double_t pTparam = 0.0; pTparam <= 0.00075; pTparam += (0.00075)/(ntoys-1)) {
+      pTparams[1].push_back(pTparam);
+      toy_name2 = toy_name2 + to_string(pTparam);
+      TH1F *hpT_toy = new TH1F(toy_name2.c_str(), "mu_PT with momentum-dependent momentum smear", hist_dims[0], hist_dims[1], hist_dims[2]);
+      toys[1].push_back(hpT_toy);
+      toy_name2 = "GausSmear_pTdependent";
+  }
+
+  
+  ofstream pT_ofs ("./pTparameters.csv", ofstream::out);
+  cout << "error marker 4" << endl;
+
+  for (int vect_row = 0; vect_row < pTparams.size(); ++vect_row) {
+    for (vector<Double_t>::iterator data_it = pTparams[vect_row].begin(); data_it != pTparams[vect_row].end(); ++data_it) { 
+      if (data_it == pTparams[vect_row].begin()) {
+	pT_ofs << *data_it; 
+      } else {
+	pT_ofs << ',' << *data_it; 
+      }
+    }
+    pT_ofs << endl;  
+  }
+  
+  pT_ofs.close();
 
   Long64_t nentries = MCDecayTree->GetEntries();
   cout << "nentries: " << nentries << endl;
   Long64_t nbytes = 0;
-  
-  int templateindex = 0;
-  int toyindex = 0;
+  int toyindex =0, templateindex=0;
 
   //  cout << "Error marker 2" << endl;
 
-  for (Long64_t eventit=0; eventit < 1000; eventit++) { //usually i< nentries*split_ratio for full data set
+  for (Long64_t eventit=0; eventit < nentries; eventit++) { //usually i< nentries*split_ratio for full data set
     nbytes += MCDecayTree->GetEntry(eventit);
     
     //cout << "error marker 3" << endl;
-    cout << "currently at " << eventit << "th iteration" << endl;
+    //cout << "currently at " << eventit << "th iteration" << endl;
 
-    if(mu_PT > 30 && mu_PT < 50){ // apply a cut
+    //if(mu_PT > 30 && mu_PT < 50){ // apply a cut
       if (eventit%2 == 0){
-	for (pTparamsit = pTparams.begin(); pTparamsit != pTparams.end(); ++pTparamsit) {
-	  //cout << "error marker 4" <<endl;
-	  toys[0]->Fill(mu_PT);
+	toyindex=0;
+	for ( auto pTparam0 : pTparams[0] ) {  // (pTparamsit = pTparams.begin(); pTparamsit != pTparams.end(); ++pTparamsit) {
+	//cout << "error marker 4" <<endl;
+	  mu_PT = mu_PT*gRandom->Gaus(1, pTparam0);
+	  toys[0][toyindex]->Fill(mu_PT);
+	  ++toyindex;
+	}
+	toyindex=0;
+	for ( auto pTparam1 : pTparams[1] ) {  // (pTparamsit = pTparams.begin(); pTparamsit != pTparams.end(); ++pTparamsit) {
+	//cout << "error marker 4" <<endl;
+	  mu_PT = mu_PT*gRandom->Gaus(1, pTparam1*mu_PT);
+	  toys[1][toyindex]->Fill(mu_PT);
+	  ++toyindex;
 	}
       } else {
-	for (Wmassit = Wmasses.begin(); Wmassit != Wmasses.end(); ++Wmassit) {
+	templateindex = 0;
+	const auto denominator = TMath::BreitWigner(prop_M, Mnom, gamma); 
+	for ( auto Wmass : Wmasses ){ //Wmassit = Wmasses.begin(); Wmassit != Wmasses.end(); ++Wmassit) {
 	  //cout << "error marker 5" <<endl;
-	  templates[templateindex]->Fill(mu_PT, TMath::BreitWigner(mu_PT, *Wmassit, gamma)/TMath::BreitWigner(mu_PT, Mnom, gamma));
-	  templateindex = (templateindex + 1) % templates.size();
+	  templates[templateindex]->Fill(mu_PT, TMath::BreitWigner(prop_M, Wmass, gamma)/denominator);
+	  //templateindex = (templateindex + 1) % templates.size();
+	  ++templateindex;
 	  //cout << "templateindex is currently: " << templateindex <<endl;
 	}
       }
-    }
+      //}
   }
  
-  cout << "Error marker 6" << endl;
-  
-  //plot_overlaid_histograms();
 
+  bool firsthist = true;
+  int colourit = 1;
+    // Below might be necessary for larger number of hypotheses
+    //int lineit = 1;
+    
+  TCanvas* c = new TCanvas("cBW", "mu PT with different W mass hypotheses");
+      
+  for (templateit = templates.begin(); templateit != templates.end(); templateit++, colourit++) {
+    (*templateit)->SetLineColor(colourit);
+
+    if(firsthist) {
+      (*templateit)->Draw();
+      firsthist = false;
+    } else (*templateit)->Draw("SAME");
+  }
+  c->Print("~/oxford-lhcb-wmass/plots/templatesHist.png");
+  c->Print("~/oxford-lhcb-wmass/plots/templatesHist.pdf");
+  c->Close();    
+
+       /*
+  TCanvas* c1 = new TCanvas("cPTsmear", "mu PT with Gaussian smear");
+      
+  for (toyit = toys[0].begin(); toyit != toys[0].end(); toyit++, colourit++) {
+    (*toyit)->SetLineColor(colourit);
+
+    if(firsthist) {
+      (*toyit)->Draw();
+      firsthist = false;
+    } else (*toyit)->Draw("SAME");
+  }
+  c1->Print("~/oxford-lhcb-wmass/plots/toys1Hist.png");
+  c1->Print("~/oxford-lhcb-wmass/plots/toys1Hist.pdf");
+  c1->Close();    
+       */
+
+  // PERFORMING A CHI SQUARE TEST
+
+  cout << "All vectors and iterators initialized.  template_vect is of length " << templates.size() << ". toy_vect is of length " <<  toys.size() << ". Wmass_vect is of length " << Wmasses.size() << endl;
+  cout << templates.front() << endl;
+  //char scaledhname[300];
+
+    // Copy template hypothesis info onto an array of fixed size to hopefully be accepted by TGraph->DrawGraph()
+  
+  //Double_t Wmass_arr[ntemplates];
+  //cout << "Copying TemplateStruct->Wmasses to an array" << endl;
+  //copy((Wmasses).begin(), (Wmasses).end(), Wmass_arr);
+  
+  TGraph *chi2Plot = new TGraph(ntemplates);
+  
+  char chi2plot_name[50];
+  char legend_header[300];
+  Double_t chi2point;
+  toyindex=0;
+  templateindex=0;  
+
+  TCanvas* c2 = new TCanvas("cchi2", "chi2 with different W mass hypotheses");
+
+  for (toyit = toys[0].begin(); toyit != toys[0].end(); toyit++) {
+    
+    cout << "performing loop over toys" << endl;
+    if ((*toyit)->GetSumw2N() == 0) {
+      cout << "Warning! Weights do not seem to be stored" << endl;
+    }
+
+    for (templateit = templates.begin(); templateit != templates.end(); templateit++){
+      
+      (*toyit)->Scale((*templateit)->Integral()/(*toyit)->Integral());
+      sprintf(chi2plot_name, "chi2plot0%u", toyindex);
+      
+      //output->WriteTObject(*toyit, scaledhname, "Overwrite");
+      chi2point = (*toyit)->Chi2Test((*templateit), "Chi2 WW");
+      
+      cout << "Copying chi2 result for toy " << toyindex << " and template " << templateindex << ": " << chi2point << endl;
+      chi2Plot->SetPoint(templateindex,Wmasses[templateindex],chi2point);
+
+      templateindex = (templateindex + 1) % templates.size();
+    }
+    
+    
+    cout << "Drawing graph of toy " << toyindex << endl;
+    chi2Plot->SetMarkerColor(4);
+    chi2Plot->SetMarkerStyle(8);
+    chi2Plot->Draw("AP");
+    
+    auto *legend = new TLegend();
+    sprintf(legend_header, "Chi2: Gaussian pT smearing %f against W mass hypotheses", pTparams[0][toyindex]);
+    legend->SetHeader(legend_header, "C");
+    legend->Draw();
+    
+    cout << "Writing chi2 plot to root file." << endl;
+    output->WriteTObject(chi2Plot, chi2plot_name, "Overwrite");
+    cout << "Write successful" << endl;
+    
+    cout << "Saving chi2 plot as an image." << endl;
+    c2->Print("./plots/chi2Plot.png");
+    c2->Clear();  
+
+    toyindex++;
+  }
+  c2->Close();
+
+  toyindex=0, templateindex=0;
+
+  TCanvas* c3 = new TCanvas("chi2", "toy: pT-dependent smearing, templates: W mass hypotheses");
+
+  for (toyit = toys[1].begin(); toyit != toys[1].end(); toyit++) {
+    
+    cout << "performing loop over toys" << endl;
+    if ((*toyit)->GetSumw2N() == 0) {
+      cout << "Warning! Weights do not seem to be stored" << endl;
+    }
+
+    for (templateit = templates.begin(); templateit != templates.end(); templateit++){
+      
+      (*toyit)->Scale((*templateit)->Integral()/(*toyit)->Integral());
+      sprintf(chi2plot_name, "chi2plot1%u", toyindex);
+      
+      //output->WriteTObject(*toyit, scaledhname, "Overwrite");
+      chi2point = (*toyit)->Chi2Test((*templateit), "Chi2 WW");
+      
+      cout << "Copying chi2 result for toy " << toyindex << " and template " << templateindex << ": " << chi2point << endl;
+      chi2Plot->SetPoint(templateindex,Wmasses[templateindex],chi2point);
+
+      templateindex = (templateindex + 1) % templates.size();
+    }
+    
+    
+    cout << "Drawing graph of toy " << toyindex << endl;
+    chi2Plot->SetMarkerColor(4);
+    chi2Plot->SetMarkerStyle(8);
+    chi2Plot->Draw("AP");
+    
+    auto *legend = new TLegend();
+    sprintf(legend_header, "Chi2: Gaussian pT smearing %f against W mass hypotheses", pTparams[0][toyindex]);
+    legend->SetHeader(legend_header, "C");
+    legend->Draw();
+    
+    cout << "Writing chi2 plot to root file." << endl;
+    output->WriteTObject(chi2Plot, chi2plot_name, "Overwrite");
+    cout << "Write successful" << endl;
+    
+    cout << "Saving chi2 plot as an image." << endl;
+    c3->Print("./plots/chi2Plot.png");
+    c3->Clear();  
+
+    toyindex++;
+  }
+  c3->Close();
+
+  
   output->Write();
   output->Close();
   return 0;
 }
+
+
